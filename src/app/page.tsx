3b71c89a6d2cf8d51725bcc71ec64843b91af4f2
yufoxda <147848207+yufoxda@users.noreply.github.com>
2025-07-03 17:09:27 +0900

    feat:ログインユーザーで登録

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { getuser } from "./actions"
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const user = await getuser();
+  
+  const handleLogout = async () => {
+    'use server';
+    redirect('/auth/logout');
+  };
+
+  const handleLogin = async () => {
+    'use server';
+    redirect('/auth/login');
+  };
+
   return (
 <div>
-  main
+  <h1>main</h1>
+  <p>Welcome to the main page!</p>
+  <div>
+    {user ? (
+      <div>
+        <p>ログイン中: {user.email}</p>
+          <button onClick={handleLogout} type="submit">
+            ログアウト
+          </button>
+      </div>
+    ) : (
+      <div>
+        <p>ログインしていません</p>
+          <button type="submit" onClick={handleLogin}>
+            ログイン
+          </button>
+      </div>
+    )}
+  </div>
 </div>
   );
 }
